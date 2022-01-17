887152aea427c742bd3bf318c6bcfb1aaac2d8e5
codingIsNotMyPassion <94367699+codingIsNotMyPassion@users.noreply.github.com>
2022-01-17 21:18:19 +0530

    Update 1.d.js

--- Day8/1.d.js
+++ Day8/1.d.js
@@ -1,3 +1,4 @@
+// Write a piece of code that creates an instance of the class Movie with the title “Casino Royale”, the studio “Eon Productions”, and the rating “PG­13”
 class Movie{
     constructor (title, studio, rating){
         this.title=title;
