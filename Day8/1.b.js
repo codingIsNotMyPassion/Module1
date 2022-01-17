eda6f85973ba04d4157f21ed416b64566519438b
codingIsNotMyPassion <94367699+codingIsNotMyPassion@users.noreply.github.com>
2022-01-17 21:16:05 +0530

    Update 1.b.js

--- Day8/1.b.js
+++ Day8/1.b.js
@@ -1,3 +1,4 @@
+// The constructor for the class Movie will set the class property rating to "PG" as default when no rating is provided
 class Movie{
     constructor (title, studio, rating){
         this.title=title;
