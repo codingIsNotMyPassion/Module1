fa85b6a185d955bd7b676cf1ca96f80f63007164
codingIsNotMyPassion <94367699+codingIsNotMyPassion@users.noreply.github.com>
2022-01-17 21:17:25 +0530

    Update 1.c.js

--- Day8/1.c.js
+++ Day8/1.c.js
@@ -1,3 +1,6 @@
+// Write a method getPG, which takes an array of base type Movie as its argument, 
+// and returns a new array of only those movies in the input array with a rating of "PG". 
+// You may assume the input array is full of Movie instances. The returned array need not be full.
 class Movie {
     constructor (title, studio, rating){
         this.title=title;
